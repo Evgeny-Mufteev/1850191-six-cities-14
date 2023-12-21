1f7c1572be779c3b8afb618ef130cbb1ddf81085
Mufteev Evgeny <evgenymufteev@gmail.com>
2023-12-21 23:25:51 +0300

    добавление исходных стилей из верстки в проект

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store/configure-store';
 import App from './app';
+import './styles/main.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
